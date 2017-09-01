7b2388b451688c01590914fd51de59ae20f37796
Hatem <zehani.hatem@gmail.com>
2017-09-01 14:41:47 +0200

    footer.js

---
+++ assets/js/footer.js
@@ -0,0 +1,42 @@
+//open popup
+$('.cdv').on('click', function(event){
+	event.preventDefault();
+	$('.cd-popup').addClass('is-visible');
+});
+
+//close popup
+$('.cd-popup').on('click', function(event){
+	if( $(event.target).is('.cd-popup-close') || $(event.target).is('.cd-popup') ) {
+		event.preventDefault();
+		$(this).removeClass('is-visible');
+	}
+});
+
+//close popup when clicking the esc keyboard button
+$(document).keyup(function(event){
+  	if(event.which==='27'){
+  		$('.cd-popup').removeClass('is-visible');
+    }
+  });
+
+
+//open popup
+$('.pdq').on('click', function(event){
+	event.preventDefault();
+	$('#cd-popup-politique').addClass('is-visible');
+});
+
+//close popup
+$('#cd-popup-politique').on('click', function(event){
+	if( $(event.target).is('.cd-popup-close') || $(event.target).is('#cd-popup-politique') ) {
+		event.preventDefault();
+		$(this).removeClass('is-visible');
+	}
+});
+
+//close popup when clicking the esc keyboard button
+$(document).keyup(function(event){
+  	if(event.which==='27'){
+  		$('#cd-popup-politique').removeClass('is-visible');
+    }
+});
---
+++ assets/js/youtube-api.js
@@ -0,0 +1,52 @@
+var videourl = $("iframe").attr("src");
+var videoindex = $("iframe").attr("src").indexOf("embed");
+
+var videoid = ("https://www.youtube.com/watch?v="+videourl.substring(videoindex + 6, videourl.length));
+
+console.log(videoid);
+
+var matches = videoid.match(/^https:\/\/www\.youtube\.com\/.*[?&]v=([^&]+)/i) || videoid.match(/^https:\/\/youtu\.be\/([^?]+)/i);
+
+if (matches) {
+  videoid = matches[1];
+}
+
+if (videoid.match(/^[a-z0-9_-]{11}$/i) === null) {
+  $(".video-title-text").text("Unable to parse Video ID/URL");
+  //return;
+}
+
+$.getJSON("https://www.googleapis.com/youtube/v3/videos",
+{
+  key: "AIzaSyAxk8yqhIqzfZ7qY2Rcs9_h4l3MU5sQjzg",
+  part: "snippet,statistics,contentDetails",
+  id: videoid
+},
+
+function(data)
+{
+  if (data.items.length === 0)
+  {
+    $(".video-title-text").text("Video not found");
+    //return;
+  }
+
+  var videodesc = data.items[0].snippet.description.replace(/\n/g, "<br /><br />");;
+
+  console.log(videodesc);
+
+  var videoduration = moment.duration(data.items[0].contentDetails.duration,moment.ISO_8601);
+
+  console.log(videoduration._milliseconds);
+
+  $(".video-title-text").text(data.items[0].snippet.title);
+  $(".video-description-text").html(videodesc);
+  $(".duration-icon").next("p").text(Math.floor(videoduration._milliseconds / 60000)
+  +":"+
+  ((videoduration._milliseconds % 60000) / 1000).toFixed(0));
+  $(".comment-icon").next("p").text(data.items[0].statistics.viewCount);
+  $(".likes-icon").next("p").text(data.items[0].statistics.likeCount);
+
+}).fail(function(jqXHR, textStatus, errorThrown) {
+  $("<p style='color: #F00;'></p>").text(jqXHR.responseText || errorThrown).appendTo("#video-data-1");
+});
